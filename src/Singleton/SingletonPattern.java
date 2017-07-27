56a41adef10fb60eed207d9f97e983c76e072fa4
Nikhil Soni <n.nikhil.ns65@gmail.com>
2017-07-27 11:50:04 +0530

    add example for singleton pattern

--- src/Singleton/SingletonPattern.java
+++ src/Singleton/SingletonPattern.java
@@ -10,5 +10,71 @@
 
 package Singleton;
 
+/**
+ * SINGLETON PATTERN HAS ONLY TWO CONSTRAINTS
+ * FIRST THAT THEIR SHOULD BE ONLY ONE INSTANCE
+ * OF THAT CLASS AND SECOND IS THAT ITS INSTANCE
+ * SHOULD HAVE A GLOBAL INSTANCE
+ * <p>
+ * WE CAN ACHIEVE THIS BY MAKING THE CONSTRUCTOR
+ * PRIVATE SO NO ONE OUTSIDE THAT CLASS CAN CREATE ITS
+ * INSTANCE AND NEXT WE CREATE A STATIC METHOD THAT
+ * STORES A NEW INSTANCE OF THAT CLASS IN A FIELD
+ * AND RETURNS THAT OBJECT WHEN EVER IT IS CALLED
+ */
+
 public class SingletonPattern {
+
+    public static void main(String... args) {
+        SingletonPattern singletonPattern = new SingletonPattern();
+        singletonPattern.getSingletonInstances();
+    }
+
+    private void getSingletonInstances() {
+        SingletonObject object1 = SingletonObject.getInstance();
+        SingletonObject object2 = SingletonObject.getInstance();
+
+        /*
+         * AFTER EXECUTION OF BOTH FUNCTION THE
+         * OBJECT1 & OBJECT2 SETTER WILL HAVE
+         * SONI SINCE THEY BOTH OPERATE ON
+         * SINGLE OBJECT
+         */
+        object1.setSetter("Nikhil");
+        object2.setSetter("Soni");
+
+        /*
+         * == CHECKS IF BOTH OBJECT POINTS TO SAME OBJECT
+         * IN HEAP HERE SINCE BOTH VARIABLE POINTS TO
+         * THE SAME OBJECT IN HEAP IT RETURNS TRUE
+         */
+        if (object1 == object2) {
+            System.out.println("Points to same object");
+        } else {
+            System.out.println("Points to different object");
+        }
+
+        /*
+         * EQUALS COMPARES WHAT IS WRITTEN TO COMPARES
+         * NOTHING MORE OR LESS HERE WE ARE COMPARING
+         * THE CONTENT OF BOTH OBJECT WHCIH ARE SAME
+         */
+        if (object1.equals(object2)) {
+            System.out.println("Contents of the object are same");
+        } else {
+            System.out.println("Contents of the object are different");
+        }
+
+        /*
+         * HERE ALSO TRUE IS RETURNED SINCE BOTH
+         * SETTER HAS THE SAME VALUE AS DISCUSSED
+         * ABOVE
+         */
+        if (object1.getSetter().equals(object2.getSetter())) {
+            System.out.println("Contents of the string are same");
+        } else {
+            System.out.println("Contents of the string are different");
+        }
+    }
+
 }
